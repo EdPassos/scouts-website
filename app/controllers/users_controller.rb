16bdbd5b45fa2beecd876638abe1774fa68e2536
EdPassos <ed.passos@gmail.com>
2015-12-03 20:10:16 +0000

    Editar Utilizador

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,29 @@
 class UsersController < ApplicationController
 	load_and_authorize_resource
 
+	before_action :set_user, only: [:update, :edit]
+
 	def index
 		@users = User.all
 	end
 
 	def edit
+	end
+
+	def update
+		if @user.update(user_params)
+			redirect_to edit_user_path(@user)
+		else
+			render "edit"
+		end
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:email, :password, :password_confirmation)
+	end
+
+	def set_user
 		@user = User.find params[:id]
 	end
 end
