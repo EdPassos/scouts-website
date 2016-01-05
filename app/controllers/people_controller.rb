2ae8d4d0e61a78b31333fddef45e845da06a4426
EdPassos <ed.passos@gmail.com>
2016-01-05 18:17:03 +0000

    Edit people

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,4 +1,6 @@
 class PeopleController < ApplicationController
+  before_action :set_person, only: [:edit, :update]
+
   def new
     @person = Person.new
   end
@@ -12,8 +14,23 @@ class PeopleController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @person.update(person_params)
+      redirect_to :back, notice: "Dados de #{@person.first_name} #{@person.last_name} actualizados"
+    else
+      render :edit
+    end
+  end
+
   private
   def person_params
     params.require(:person).permit(:first_name, :last_name, :middle_names, :birthday)
+  end
+
+  def set_person
+    @person = Person.find params[:id]
   end
 end
