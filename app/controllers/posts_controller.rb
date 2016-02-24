8d2c92a7ce60b6c637d9da801460911420160d69
EdPassos <ed.passos@gmail.com>
2016-02-24 23:57:27 +0000

    Retabed posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,45 +1,45 @@
 class PostsController < ApplicationController
-	load_and_authorize_resource
+  load_and_authorize_resource
 
-	before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show, :edit, :update]
 
-	def index
-		@posts = Post.all
-	end
+  def index
+    @posts = Post.all
+  end
 
-	def show
-	end
+  def show
+  end
 
-	def new
-		@post = Post.new
-	end
+  def new
+    @post = Post.new
+  end
 
-	def create
-		@post = current_user.posts.new(post_params)
-		if @post.save
-			redirect_to @post, notice: "Artigo '#{@post.title}' publicado"
-		else
-			render :new
-		end
-	end
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to @post, notice: "Artigo '#{@post.title}' publicado"
+    else
+      render :new
+    end
+  end
 
-	def edit
-	end
+  def edit
+  end
 
-	def update
-		if @post.update(post_params)
-			redirect_to @post
-		else
-			render :edit
-		end
-	end
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
 
-	private
-	def post_params
-		params.require(:post).permit(:title, :body)
-	end
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 
-	def set_post
-		@post = Post.find params[:id]
-	end
+  def set_post
+    @post = Post.find params[:id]
+  end
 end
