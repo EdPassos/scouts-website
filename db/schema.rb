e630a2e5e1a59e6092fe0619073c5ba88eee7b52
Ed Passos <ed.passos@gmail.com>
2016-02-25 17:08:49 +0100

    Added sqlite for dev work

--- db/schema.rb
+++ db/schema.rb
@@ -14,127 +14,120 @@
 ActiveRecord::Schema.define(version: 20160105193124) do
 
   create_table "events", force: :cascade do |t|
-    t.string   "title",       limit: 255
-    t.text     "description", limit: 65535
+    t.string   "title"
+    t.text     "description"
     t.datetime "date"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "people", force: :cascade do |t|
-    t.string   "first_name",         limit: 255
-    t.string   "last_name",          limit: 255
-    t.string   "middle_names",       limit: 255
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "middle_names"
     t.date     "birthday"
-    t.integer  "caretaker_id",       limit: 4
-    t.integer  "user_id",            limit: 4
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
-    t.string   "photo_file_name",    limit: 255
-    t.string   "photo_content_type", limit: 255
-    t.integer  "photo_file_size",    limit: 4
+    t.integer  "caretaker_id"
+    t.integer  "user_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
   end
 
-  add_index "people", ["user_id"], name: "index_people_on_user_id", using: :btree
+  add_index "people", ["user_id"], name: "index_people_on_user_id"
 
   create_table "posts", force: :cascade do |t|
-    t.string   "title",      limit: 255
-    t.text     "body",       limit: 65535
-    t.integer  "user_id",    limit: 4
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.string   "title"
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "roles", force: :cascade do |t|
-    t.string   "name",          limit: 255
-    t.integer  "resource_id",   limit: 4
-    t.string   "resource_type", limit: 255
+    t.string   "name"
+    t.integer  "resource_id"
+    t.string   "resource_type"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id", using: :btree
-  add_index "roles", ["name"], name: "index_roles_on_name", using: :btree
+  add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
+  add_index "roles", ["name"], name: "index_roles_on_name"
 
   create_table "shop_order_statuses", force: :cascade do |t|
-    t.string   "name",        limit: 255
-    t.text     "description", limit: 65535
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "shop_orders", force: :cascade do |t|
-    t.integer  "shop_order_status_id", limit: 4
-    t.datetime "created_at",                       null: false
-    t.datetime "updated_at",                       null: false
-    t.integer  "user_id",              limit: 4
-    t.string   "proof_file_name",      limit: 255
-    t.string   "proof_content_type",   limit: 255
-    t.integer  "proof_file_size",      limit: 4
+    t.integer  "shop_order_status_id"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+    t.integer  "user_id"
+    t.string   "proof_file_name"
+    t.string   "proof_content_type"
+    t.integer  "proof_file_size"
     t.datetime "proof_updated_at"
   end
 
-  add_index "shop_orders", ["shop_order_status_id"], name: "index_shop_orders_on_shop_order_status_id", using: :btree
-  add_index "shop_orders", ["user_id"], name: "index_shop_orders_on_user_id", using: :btree
+  add_index "shop_orders", ["shop_order_status_id"], name: "index_shop_orders_on_shop_order_status_id"
+  add_index "shop_orders", ["user_id"], name: "index_shop_orders_on_user_id"
 
   create_table "shop_products", force: :cascade do |t|
-    t.string   "name",               limit: 255
-    t.text     "description",        limit: 65535
-    t.decimal  "price",                            precision: 7, scale: 2
-    t.datetime "created_at",                                               null: false
-    t.datetime "updated_at",                                               null: false
-    t.string   "photo_file_name",    limit: 255
-    t.string   "photo_content_type", limit: 255
-    t.integer  "photo_file_size",    limit: 4
+    t.string   "name"
+    t.text     "description"
+    t.decimal  "price",              precision: 7, scale: 2
+    t.datetime "created_at",                                 null: false
+    t.datetime "updated_at",                                 null: false
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
   end
 
   create_table "shopping_cart_products", force: :cascade do |t|
-    t.integer  "user_id",         limit: 4
-    t.integer  "shop_product_id", limit: 4
-    t.integer  "quantity",        limit: 4
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
-    t.integer  "shop_order_id",   limit: 4
+    t.integer  "user_id"
+    t.integer  "shop_product_id"
+    t.integer  "quantity"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.integer  "shop_order_id"
   end
 
-  add_index "shopping_cart_products", ["shop_order_id"], name: "index_shopping_cart_products_on_shop_order_id", using: :btree
-  add_index "shopping_cart_products", ["shop_product_id"], name: "index_shopping_cart_products_on_shop_product_id", using: :btree
-  add_index "shopping_cart_products", ["user_id"], name: "index_shopping_cart_products_on_user_id", using: :btree
+  add_index "shopping_cart_products", ["shop_order_id"], name: "index_shopping_cart_products_on_shop_order_id"
+  add_index "shopping_cart_products", ["shop_product_id"], name: "index_shopping_cart_products_on_shop_product_id"
+  add_index "shopping_cart_products", ["user_id"], name: "index_shopping_cart_products_on_user_id"
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "users_roles", id: false, force: :cascade do |t|
-    t.integer "user_id", limit: 4
-    t.integer "role_id", limit: 4
+    t.integer "user_id"
+    t.integer "role_id"
   end
 
-  add_index "users_roles", ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id", using: :btree
+  add_index "users_roles", ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
 
-  add_foreign_key "people", "users"
-  add_foreign_key "posts", "users"
-  add_foreign_key "shop_orders", "shop_order_statuses"
-  add_foreign_key "shop_orders", "users"
-  add_foreign_key "shopping_cart_products", "shop_orders"
-  add_foreign_key "shopping_cart_products", "shop_products"
-  add_foreign_key "shopping_cart_products", "users"
 end
