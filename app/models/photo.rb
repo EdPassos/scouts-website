3080143d506e85758ed8ef7619a72f71ca5e7c41
Ed <prot3us@debie.debie>
2016-02-28 03:57:26 +0000

    new image size album listing is better

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ActiveRecord::Base
   has_attached_file :file,
-    styles: { medium: "300x300>", thumb: "100x100>" },
+    styles: { medium: "300x300>", thumb: "100x100>", large: "800x800>" },
     default_url: "/images/:style/missing.png"
   validates_attachment_content_type :file, content_type: /\Aimage\/.*\Z/
   validates :file, attachment_presence: true
--- app/models/photo_album.rb
+++ app/models/photo_album.rb
@@ -2,4 +2,13 @@ class PhotoAlbum < ActiveRecord::Base
   has_many :photos, dependent: :destroy
 
   accepts_nested_attributes_for :photos
+
+  def cover
+    photo = self.photos.first
+    if photo
+      photo
+    else
+      Photo.new
+    end
+  end
 end
