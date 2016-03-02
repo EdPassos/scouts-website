c0c4c9a4614ed9fa8de9e3db9a5eafc4a53235e6
EdPassos <ed.passos@gmail.com>
2016-03-02 21:07:14 +0000

    Few topbar active buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  def button_class(ctrls = [], actions = [])
+    ctrls.each do |controller|
+      if controller.eql? params[:controller]
+        if actions.blank?
+          return 'active'
+        end
+        actions.each do |action|
+          if action.eql? params[:action]
+            return 'active'
+          end
+        end
+      end
+    end
+    return ''
+  end
+
 end
