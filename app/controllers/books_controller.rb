3bbfdcf1b952a6a1e66b79cd0559e399d7246b48
EdPassos <ed.passos@gmail.com>
2016-04-04 12:45:10 +0100

    Book cover

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -71,6 +71,6 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:title, :author, :borrowed_to_id)
+      params.require(:book).permit(:title, :author, :borrowed_to_id, :cover)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160403195058) do
+ActiveRecord::Schema.define(version: 20160404113535) do
 
   create_table "adventures", force: :cascade do |t|
     t.string   "name"
@@ -38,8 +38,12 @@ ActiveRecord::Schema.define(version: 20160403195058) do
     t.string   "title"
     t.string   "author"
     t.integer  "borrowed_to_id"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "cover_file_name"
+    t.string   "cover_content_type"
+    t.integer  "cover_file_size"
+    t.datetime "cover_updated_at"
   end
 
   add_index "books", ["borrowed_to_id"], name: "index_books_on_borrowed_to_id"
