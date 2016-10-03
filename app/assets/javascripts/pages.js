575bb9ee256fabe2cd2835f4f69fa04d3489e74b
EdPassos <ed.passos@gmail.com>
2016-10-03 02:43:55 +0100

    That Topbar though

---
+++ app/assets/javascripts/pages.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function () {
+  console.log('hello');
+
+  var topbar = $('#topbar');
+
+  $(window).scroll(function(e) {
+    var min_scroll_amount = 0;
+    var scroll_amount = $(this).scrollTop();
+
+    if(scroll_amount > min_scroll_amount) {
+      topbar.addClass('scrolled');
+    } else {
+      topbar.removeClass('scrolled');
+    }
+  });
+});
