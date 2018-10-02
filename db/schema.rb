5b0495e93db14e2ffa5a0fec4017de782538462d
Eduardo Passos <ed.passos@gmail.com>
2018-10-02 01:46:58 +0100

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,220 +10,220 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160404113535) do
+ActiveRecord::Schema.define(version: 2016_04_04_113535) do
 
-  create_table "adventures", force: :cascade do |t|
-    t.string   "name"
-    t.date     "start"
-    t.date     "end"
-    t.bigint  "photo_album_id"
-    t.bigint  "post_id"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+  create_table "adventures", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.date "start"
+    t.date "end"
+    t.bigint "photo_album_id"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["photo_album_id"], name: "index_adventures_on_photo_album_id"
     t.index ["post_id"], name: "index_adventures_on_post_id"
   end
 
-  create_table "adventures_people", id: false, force: :cascade do |t|
+  create_table "adventures_people", id: false, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "adventure_id"
     t.bigint "person_id"
     t.index ["adventure_id"], name: "index_adventures_people_on_adventure_id"
     t.index ["person_id"], name: "index_adventures_people_on_person_id"
   end
 
-  create_table "books", force: :cascade do |t|
-    t.string   "title"
-    t.string   "author"
-    t.bigint  "borrowed_to_id"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
-    t.string   "cover_file_name"
-    t.string   "cover_content_type"
-    t.integer  "cover_file_size"
+  create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.bigint "borrowed_to_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "cover_file_name"
+    t.string "cover_content_type"
+    t.integer "cover_file_size"
     t.datetime "cover_updated_at"
     t.index ["borrowed_to_id"], name: "index_books_on_borrowed_to_id"
   end
 
-  create_table "camp_nights", force: :cascade do |t|
-    t.bigint  "adventure_id"
-    t.bigint  "person_id"
-    t.integer  "nights"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+  create_table "camp_nights", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "adventure_id"
+    t.bigint "person_id"
+    t.integer "nights"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["adventure_id"], name: "index_camp_nights_on_adventure_id"
     t.index ["person_id"], name: "index_camp_nights_on_person_id"
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "name"
-    t.text     "description"
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.text "description"
   end
 
-  create_table "categories_posts", force: :cascade do |t|
+  create_table "categories_posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "category_id"
     t.bigint "post_id"
     t.index ["category_id"], name: "index_categories_posts_on_category_id"
     t.index ["post_id"], name: "index_categories_posts_on_post_id"
   end
 
-  create_table "events", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
+  create_table "events", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
     t.datetime "date"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "people", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "middle_names"
-    t.date     "birthday"
-    t.bigint  "caretaker_id"
-    t.bigint  "user_id"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
-    t.string   "photo_file_name"
-    t.string   "photo_content_type"
-    t.integer  "photo_file_size"
+  create_table "people", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "middle_names"
+    t.date "birthday"
+    t.bigint "caretaker_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "photo_file_name"
+    t.string "photo_content_type"
+    t.integer "photo_file_size"
     t.datetime "photo_updated_at"
-    t.bigint  "section_id"
-    t.bigint  "team_id"
+    t.bigint "section_id"
+    t.bigint "team_id"
     t.index ["section_id"], name: "index_people_on_section_id"
     t.index ["team_id"], name: "index_people_on_team_id"
     t.index ["user_id"], name: "index_people_on_user_id"
   end
 
-  create_table "photo_albums", force: :cascade do |t|
-    t.string   "name"
+  create_table "photo_albums", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
     t.datetime "start"
     t.datetime "end"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "photos", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
-    t.string   "file_file_name"
-    t.string   "file_content_type"
-    t.integer  "file_file_size"
+  create_table "photos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "file_file_name"
+    t.string "file_content_type"
+    t.integer "file_file_size"
     t.datetime "file_updated_at"
-    t.bigint  "photo_album_id"
+    t.bigint "photo_album_id"
     t.index ["photo_album_id"], name: "index_photos_on_photo_album_id"
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.bigint  "user_id"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
     t.datetime "image_updated_at"
-    t.boolean  "draft"
+    t.boolean "draft"
     t.datetime "published_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
-  create_table "roles", force: :cascade do |t|
-    t.string   "name"
-    t.string   "resource_type"
-    t.bigint  "resource_id"
+  create_table "roles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "resource_type"
+    t.bigint "resource_id"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
     t.index ["name"], name: "index_roles_on_name"
   end
 
-  create_table "sections", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
-    t.string   "banner_file_name"
-    t.string   "banner_content_type"
-    t.integer  "banner_file_size"
+  create_table "sections", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "banner_file_name"
+    t.string "banner_content_type"
+    t.integer "banner_file_size"
     t.datetime "banner_updated_at"
-    t.boolean  "public"
+    t.boolean "public"
   end
 
-  create_table "shop_order_statuses", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+  create_table "shop_order_statuses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "shop_orders", force: :cascade do |t|
-    t.bigint  "shop_order_status_id"
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
-    t.bigint  "user_id"
-    t.string   "proof_file_name"
-    t.string   "proof_content_type"
-    t.integer  "proof_file_size"
+  create_table "shop_orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "shop_order_status_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.string "proof_file_name"
+    t.string "proof_content_type"
+    t.integer "proof_file_size"
     t.datetime "proof_updated_at"
     t.index ["shop_order_status_id"], name: "index_shop_orders_on_shop_order_status_id"
     t.index ["user_id"], name: "index_shop_orders_on_user_id"
   end
 
-  create_table "shop_products", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.decimal  "price",              precision: 7, scale: 2
-    t.datetime "created_at",                                 null: false
-    t.datetime "updated_at",                                 null: false
-    t.string   "photo_file_name"
-    t.string   "photo_content_type"
-    t.integer  "photo_file_size"
+  create_table "shop_products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.decimal "price", precision: 7, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "photo_file_name"
+    t.string "photo_content_type"
+    t.integer "photo_file_size"
     t.datetime "photo_updated_at"
   end
 
-  create_table "shopping_cart_products", force: :cascade do |t|
-    t.bigint  "user_id"
-    t.bigint  "shop_product_id"
-    t.integer  "quantity"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.bigint  "shop_order_id"
+  create_table "shopping_cart_products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "shop_product_id"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "shop_order_id"
     t.index ["shop_order_id"], name: "index_shopping_cart_products_on_shop_order_id"
     t.index ["shop_product_id"], name: "index_shopping_cart_products_on_shop_product_id"
     t.index ["user_id"], name: "index_shopping_cart_products_on_user_id"
   end
 
-  create_table "teams", force: :cascade do |t|
-    t.string   "name"
+  create_table "teams", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint  "section_id"
+    t.bigint "section_id"
     t.index ["section_id"], name: "index_teams_on_section_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "users_roles", id: false, force: :cascade do |t|
+  create_table "users_roles", id: false, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "role_id"
     t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
